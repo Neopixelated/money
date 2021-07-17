815b4f0922027b75488f342689ecb5fe39bd3f81
Neo <apnizamov@gmail.com>
2021-07-17 15:20:51 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print "I am money"
+print("---Money caluclator---")
+
+first = input("First money=")
+second = input("Second money=")
+answer = float(first) + float(second)
+print("money=" + str(answer))
+print("----------------------")
